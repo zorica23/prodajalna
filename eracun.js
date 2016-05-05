55eb2c66a466ed098c2fd081d83dde2cc0246e6d
zorica ilievska <zi3601@student.uni-lj.si>
2016-05-05 14:42:28 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -209,13 +209,29 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address,polja.City , polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
       stmt.finalize();
     } catch (err) {
       napaka2 = true;
     }
   
-    odgovor.end();
+    
+    if(napaka2){
+      vrniStranke(function(napaka1, stranke){ 
+          vrniRacune(function(napaka2, racuni){ 
+            odgovor.render('prijava',{sporocilo: "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.", seznamStrank: stranke, seznamRacunov: racuni});
+
+        })
+      })
+    }else{
+      vrniStranke(function(napaka1, stranke) {
+          vrniRacune(function(napaka2, racuni) {
+            odgovor.render('prijava', {sporocilo: "Stranka je bila uspešno registrirana.", seznamStrank: stranke, seznamRacunov: racuni});
+          })
+      })
+    }
+    
+    //odgovor.end();
   });
 })
 
