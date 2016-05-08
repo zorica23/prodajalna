a1647534ed017f6b6c5a681af3c584de647c2d48
zorica ilievska <zi3601@student.uni-lj.si>
2016-05-08 12:00:49 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,10 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+       if(napaka)
+        callback(false);
+      else
+        callback(vrstice);
     })
 }
 
@@ -142,14 +145,28 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
-    })
+      if(napaka)
+        callback(false);
+      else
+        callback(vrstice);
+    });
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
-})
+    var form = new formidable.IncomingForm();
+    
+    form.parse(zahteva, function(napaka1,polja,datoteke) {
+      
+      strankaIzRacuna(polja.seznamRacunov, function(stranka) {
+          pesmiIzRacuna(polja.seznamRacunov,function(pesmi) {
+            odgovor.setHeader('content-type', 'text/xml');
+            odgovor.render('eslog', {vizualiziraj:true, postavkeRacuna: pesmi, narocnikRacuna: stranka[0]});
+          });
+        
+      });
+    });
+});
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
 streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
